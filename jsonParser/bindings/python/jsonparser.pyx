cde66c9431168c31452dc0677d3c3e2e13d4d835
Lindsay <lindsayjamesmartin94@gmail.com>
2023-04-10 00:46:54 +0200

    Start fresh in Unity
    Using MLX was going to be more of a pain than a time saver, and Thijs showed me an amazing youtube series doing something very similar to what I wanted in Unity, which is exactly the engine I wanted to switch to once I had my prototype. Sorry Leon!

--- jsonParser/bindings/python/jsonparser.pyx
+++
@@ -1,9 +0,0 @@
-cdef extern from "wrap_json.c":
-    object decode_json(char * value)
-    object get_exception_class()
-
-JSONException = get_exception_class()
-
-def decode(value):
-    value = value.encode('utf-8')
-    return decode_json(value)
