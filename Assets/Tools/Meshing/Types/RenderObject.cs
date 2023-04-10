2719e5bd7d8b71da215eddb6074ff135d77531a2
Lindsay <lindsayjamesmartin94@gmail.com>
2023-04-10 22:47:56 +0200

    Make junk files to generate cube
    I made a bunch of junk files and started pulling stuff from Seb's repo to see what it would take to actually visibly *see* the mesh that had just been coded used on a game object. But I was needing to take more and more things and may have been looking in the wrong place, so now have gone back to his earlier Procedural Planets video to see how the cube sphere is first generated there.

---
+++ Assets/Tools/Meshing/Types/RenderObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct RenderObject
+{
+	//public readonly GameObject gameObject;
+	//public readonly MeshRenderer renderer;
+	//public readonly MeshFilter filter;
+	//public readonly Material material;
+
+	//public RenderObject(GameObject gameObject, MeshRenderer renderer, MeshFilter filter, Material material)
+	//{
+	//	this.gameObject = gameObject;
+	//	this.renderer = renderer;
+	//	this.filter = filter;
+	//	this.material = material;
+	//}
+}
