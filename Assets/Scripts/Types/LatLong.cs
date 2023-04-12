9bb5a8e952dee5e46fb498bdb00b7e8a551ffc8d
Lindsay <lindsayjamesmartin94@gmail.com>
2023-04-12 23:31:14 +0200

    Add struct to store Latitude & Longitude
    Still empty though.

---
+++ Assets/Scripts/Types/LatLong.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct LatLong
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
