959c6af612129d4ea89212eed7de5b89a552e4d0
Lindsay <lindsayjamesmartin94@gmail.com>
2023-04-28 11:34:27 +0200

    Complete FileHelper class

--- Assets/Tools/Extra/FileHelper/FileHelper.cs
+++ Assets/Tools/Extra/FileHelper/FileHelper.cs
@@ -1,8 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public static class FileHelper
 {
+    public static string MakePath(params string[] dirNames)
+    {
+        return Path.Combine(dirNames);
+    }
 
+    public static string PersistentDataPath
+    {
+        get
+        {
+            return Application.persistentDataPath;
+        }
+    }
+
+    public static void SaveTextToFile(string path, 
+                                      string fileName, 
+                                      string fileExtension,
+                                      string data,
+                                      bool log = false)
+    {
+        if (fileExtension[0] != '.')
+        {
+            fileExtension = "." + fileExtension;
+        }
+        string file = Path.Combine(path, fileName + fileExtension);
+        SaveTextToFile(file, data, log);
+    }
+
+    public static void SaveTextToFile(string fullPath, string data, bool log = false)
+    {
+        StreamWriter writer = new StreamWriter(File.Open(fullPath, FileMode.Create));
+        writer.Write(data);
+
+        if (log)
+            Debug.Log("Wrote to: " + fullPath);
+    }
 }
