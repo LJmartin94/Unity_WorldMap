02bb5906c11bba6995ed364c710ae61b91ad7d8b
Lindsay <lindsayjamesmartin94@gmail.com>
2023-04-28 12:02:43 +0200

    Add empty TerrainGenerator class
    Based on abstract Generator class

---
+++ Assets/Scripts/Generation/Terrain/TerrainGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : Generator
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    public override void StartGenerating()
+    {
+    }
+    public override void Save()
+    {
+    }
+    public override void Load()
+    {
+    }
+}
