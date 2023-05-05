593a5a218c45bb4e5c1ca0e7ea08c7e10025e560
Lindsay <lindsayjamesmartin94@gmail.com>
2023-05-05 21:45:06 +0200

    Add Dispatch and GetThreadGroupSizes

--- Assets/Tools/Compute/ComputeHelper.cs
+++ Assets/Tools/Compute/ComputeHelper.cs
@@ -15,24 +15,25 @@ public static class ComputeHelper
     //static ComputeShader copy3DCompute;
     //static Shader bicubicUpscale;
 
-    ///// Convenience method for dispatching a compute shader.
-    ///// It calculates the number of thread groups based on the number of iterations needed.
-    //public static void Dispatch(ComputeShader cs, int numIterationsX, int numIterationsY = 1, int numIterationsZ = 1, int kernelIndex = 0)
-    //{
-    //	Vector3Int threadGroupSizes = GetThreadGroupSizes(cs, kernelIndex);
-    //	int numGroupsX = Mathf.CeilToInt(numIterationsX / (float)threadGroupSizes.x);
-    //	int numGroupsY = Mathf.CeilToInt(numIterationsY / (float)threadGroupSizes.y);
-    //	int numGroupsZ = Mathf.CeilToInt(numIterationsZ / (float)threadGroupSizes.y);
-    //	cs.Dispatch(kernelIndex, numGroupsX, numGroupsY, numGroupsZ);
-    //}
 
-    ///// Convenience method for dispatching a compute shader.
-    ///// It calculates the number of thread groups based on the size of the given texture.
-    //public static void Dispatch(ComputeShader cs, RenderTexture texture, int kernelIndex = 0)
-    //{
-    //	Vector3Int threadGroupSizes = GetThreadGroupSizes(cs, kernelIndex);
-    //	Dispatch(cs, texture.width, texture.height, texture.volumeDepth, kernelIndex);
-    //}
+    /// Convenience method for dispatching a compute shader.
+    /// It calculates the number of thread groups based on the size of the given texture.
+    public static void Dispatch(ComputeShader cs, RenderTexture texture, int kernelIndex = 0)
+    {
+        //Vector3Int threadGroupSizes = GetThreadGroupSizes(cs, kernelIndex);
+        Dispatch(cs, texture.width, texture.height, texture.volumeDepth, kernelIndex);
+    }
+
+    /// Convenience method for dispatching a compute shader.
+    /// It calculates the number of thread groups based on the number of iterations needed.
+    public static void Dispatch(ComputeShader cs, int numIterationsX, int numIterationsY = 1, int numIterationsZ = 1, int kernelIndex = 0)
+    {
+        Vector3Int threadGroupSizes = GetThreadGroupSizes(cs, kernelIndex);
+        int numGroupsX = Mathf.CeilToInt(numIterationsX / (float)threadGroupSizes.x);
+        int numGroupsY = Mathf.CeilToInt(numIterationsY / (float)threadGroupSizes.y);
+        int numGroupsZ = Mathf.CeilToInt(numIterationsZ / (float)threadGroupSizes.y);
+        cs.Dispatch(kernelIndex, numGroupsX, numGroupsY, numGroupsZ);
+    }
 
     //public static void Dispatch(ComputeShader cs, Texture2D texture, int kernelIndex = 0)
     //{
@@ -172,12 +173,12 @@ public static class ComputeHelper
         }
     }
 
-    //public static Vector3Int GetThreadGroupSizes(ComputeShader compute, int kernelIndex = 0)
-    //{
-    //	uint x, y, z;
-    //	compute.GetKernelThreadGroupSizes(kernelIndex, out x, out y, out z);
-    //	return new Vector3Int((int)x, (int)y, (int)z);
-    //}
+    public static Vector3Int GetThreadGroupSizes(ComputeShader compute, int kernelIndex = 0)
+    {
+        uint x, y, z;
+        compute.GetKernelThreadGroupSizes(kernelIndex, out x, out y, out z);
+        return new Vector3Int((int)x, (int)y, (int)z);
+    }
 
     //// ------ Texture Helpers ------
 
